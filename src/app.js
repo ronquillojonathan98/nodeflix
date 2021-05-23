804daef30a045cc5a2d77a54a340b569479dc6a1
Jonathan P. Ronquillo <ronquillojonathan98>
2021-05-23 22:43:32 +0800

    Latest Update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+const path = require('path')
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+
+const port = process.env.port() || 2021
+
+console.log(port)
+
+app.use(routes)
+
+app.set('views engine', 'hbs')
+
+app.listen('2021', () => console.log('Listening on port 2021'))
